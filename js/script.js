53503f17f7c1cc0876cc8914a9eccda1d13b7045
Guido Stocco <guidostoccodev@gmail.com>
2024-06-27 20:40:48 -0300

    Selecionando elementos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Selecionar elementos
+const inputElement = document.querySelector("#input");
+const fromElement = document.querySelector("#from");
+const toElement = document.querySelector("#to");
+const outputElement = document.querySelector("#output");
+const btnElement = document.querySelector(".convert-btn");
+const messageElement = document.querySelector("#message");
