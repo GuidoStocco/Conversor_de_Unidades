9f08dc21c756355bae9d58ba3c158dd3b120b92a
Guido Stocco <guidostoccodev@gmail.com>
2024-06-27 20:56:22 -0300

    convertendo a entrada para metros

--- js/script.js
+++ js/script.js
@@ -5,3 +5,45 @@ const toElement = document.querySelector("#to");
 const outputElement = document.querySelector("#output");
 const btnElement = document.querySelector(".convert-btn");
 const messageElement = document.querySelector("#message");
+
+
+
+//Função para converter as unidades
+
+function convert() {
+
+    const fromValue = fromElement.value;
+    const toValue = toElement.value;
+
+    if(fromValue === toValue){
+        outputElement.value = inputElement.value;
+        messageElement.textContent = "";
+        return;
+    }
+
+
+    // Converter a entrada para metros
+    let meters;
+
+    switch(fromValue){
+        case "m":
+            meters = inputElement.value;
+            break
+        case "km":
+            meters = inputElement.value * 1000
+            break    
+        case "cm":
+            meters = inputElement.value / 100
+            break        
+        case "mn":
+            meters = inputElement.value / 1000
+            break       
+    }
+}
+
+
+btnElement.addEventListener("click", (e) => {
+    e.preventDefault();
+    convert();
+
+})
